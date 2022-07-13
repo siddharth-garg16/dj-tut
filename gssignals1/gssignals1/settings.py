96a70dae60bebe2a944af1eecd14eab9000f904d
Siddharth Garg <80592648+siddharth-garg16@users.noreply.github.com>
2022-07-14 00:03:35 +0530

    Update settings.py

--- gssignals1/gssignals1/settings.py
+++ gssignals1/gssignals1/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6p(61by)#=7fpamv0m!p0t$mky@gl7b@!hd=&fm-k6l3^uyvan'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
